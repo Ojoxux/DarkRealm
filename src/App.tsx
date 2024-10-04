2602f983aec5096cc786b8807354226351678c52
Jou Okuyama <ojoxux@gmail.com>
2024-10-05 04:19:34 +0900

    refac♻️: App.tsxにカスタムテーマを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { LoginPage } from "../src/components/pages/LoginPage";
+import theme from "./theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <LoginPage />
     </ChakraProvider>
   );
