63df2e048be3839dd9366be1d58c45c2fdaf5a35
Jou Okuyama <ojoxux@gmail.com>
2024-10-05 04:18:05 +0900

    style🎨: Organismsコンポーネントのスタイル調整

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -1,17 +1,25 @@
-import { VStack } from "@chakra-ui/react";
+import { VStack, Text, Link } from "@chakra-ui/react";
 import { FormField } from "../molecules/FormField";
 import { CustomButton } from "../atoms/CustomButton";
 
 export const LoginForm: React.FC = () => (
   <VStack spacing={4} align="stretch">
     <FormField
-      label="Email"
-      inputProps={{ type: "email", placeholder: "Enter your email" }}
+      label="Email address"
+      inputProps={{ placeholder: "Enter your email" }}
     />
     <FormField
       label="Password"
-      inputProps={{ type: "password", placeholder: "Enter your password" }}
+      inputProps={{ type: "password", placeholder: "Password" }}
     />
-    <CustomButton>Log In</CustomButton>
+    <CustomButton size="lg" width="100%">
+      LOG IN
+    </CustomButton>
+    <Text textAlign="center" fontSize="sm">
+      Don't have an account?{" "}
+      <Link color="brand.500" fontWeight="bold">
+        SIGN UP
+      </Link>
+    </Text>
   </VStack>
 );
