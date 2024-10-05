f3c525684304aac3bf4b450368f606bc1915b17e
Jou Okuyama <ojoxux@gmail.com>
2024-10-05 12:33:34 +0900

    style🎨: AuthTemplateコンポーネントのレイアウトとスタイルを調整し、UIを改善しました。

--- src/components/templates/AuthTemplate.tsx
+++ src/components/templates/AuthTemplate.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Heading, VStack } from "@chakra-ui/react";
+import { Box, Container, Heading, VStack, Text } from "@chakra-ui/react";
 
 interface AuthTemplateProps {
   title: string;
@@ -9,22 +9,43 @@ export const AuthTemplate: React.FC<AuthTemplateProps> = ({
   title,
   children,
 }) => (
-  <Container
-    maxW="100%"
-    centerContent
-    py={10}
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-  >
-    <VStack spacing={10} width="100%" maxW="xl">
+  <Container maxW="100vw" display="flex" height="100vh" px={0}>
+    <VStack
+      spacing={10}
+      width="50%"
+      maxW="2xl"
+      flex="1"
+      p={8}
+      bg="gray.800"
+      justifyContent="center"
+      marginLeft="10"
+      marginRight="10"
+    >
       <Heading as="h1" size="3xl" textAlign="center">
         {title}
       </Heading>
-      <Box width="100%" p={12} borderRadius="xl" bg="gray.700">
+      <Box width="100%" p={8} borderRadius="xl" bg="gray.700">
         {children}
       </Box>
     </VStack>
+    <Box
+      flex="1"
+      bg="brand.500"
+      color="white"
+      display="flex"
+      justifyContent="center"
+      alignItems="flex-start"
+      pt={130}
+    >
+      <Heading as="h2" size="4xl" textAlign="center">
+        <Text as="span" fontWeight="normal">
+          Welcome back to
+        </Text>
+        <br />
+        <Text as="span" fontWeight="bold">
+          "DarkRealm!"
+        </Text>
+      </Heading>
+    </Box>
   </Container>
 );
