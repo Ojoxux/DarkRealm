8c895d9b26c9fece76e3c527f198df11cf4a0d4d
Jou Okuyama <ojoxux@gmail.com>
2025-01-02 17:17:26 +0900

    chore: ESLint設定を更新し、TypeScript対応、Prettier統合を強化、無視するファイルとルールを調整してコードの一貫性の向上

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,40 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ["dist"],
-    files: ["**/*.{ts,tsx}"],
+    ignores: ['dist', 'src-tauri/target/**'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: 2020,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
-        ...globals.es2021, // ここでES2021のグローバル変数を追加
+        ...globals.es2021,
+        React: true,
       },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "@typescript-eslint": tseslint,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
       prettier: prettier,
     },
     settings: {
       react: {
-        version: "detect", // Reactのバージョンを自動検出
+        version: 'detect',
       },
     },
     rules: {
@@ -35,11 +42,19 @@ export default [
       ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...prettierConfig.rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "prettier/prettier": "error",
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'es5',
+          semi: true,
+          tabWidth: 2,
+        },
+      ],
     },
   },
 ];
